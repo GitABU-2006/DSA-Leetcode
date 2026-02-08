d7eb155f15b6844c1e57802f66ed177a756e5b26
ABU BAKAR <154548399+GitABU-2006@users.noreply.github.com>
2026-02-08 10:43:08 +0530

    Time: 2090 ms (17.97%), Space: 7.8 MB (20.89%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum = 0 ; 
+        int sum = 0 ;
+        bool check = true; 
         for(int i = 1 ; i<num ; i++){
             if(num%i==0){
                 sum += i;
             }
         }
-        if(sum==num){
+        if(sum != num){
+            check = false;
+        }
+        if(check){
             return true;
         }
         else{
-            return false;
+           return false;
         }
+       
     }
 };

910a313f33b9d47f86fe2d3d8f310a163d4b0b20
ABU BAKAR <154548399+GitABU-2006@users.noreply.github.com>
2026-02-13 23:21:12 +0530

    Time: 13 ms (25.67%), Space: 55 MB (85.16%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int size = nums.size();
         int k = 1 ;
-        for(int i =0 ; i<size ; i++){
+        for(int i =0 ; i<nums.size() ; i++){
             if(nums[i]==k){
                 k++;
             }

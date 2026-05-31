f9e2fd2297a69f24ed404a69e8de6e8142794ce8
ABU BAKAR <154548399+GitABU-2006@users.noreply.github.com>
2026-05-31 23:55:19 +0530

    Time: 27 ms (84.23%), Space: 106.7 MB (81.61%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(), asteroids.end());
-        bool flag = false ; 
         long long c = mass;
         for(int i = 0 ; i<asteroids.size() ; i++){
             if(c >= asteroids[i]){
                 c+=asteroids[i];
-                flag = true ;
             }
             else{
-                flag = false;
-                break ;
+                 return false;
             }
 
         }
-        if(flag){
             return true ;
-        }
-        return false;
     }
 };
